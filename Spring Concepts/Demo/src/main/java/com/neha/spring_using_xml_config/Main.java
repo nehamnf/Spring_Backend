4d14fd446ab6b20251c5d72497c6ec313fa66678
NehaMN <nehamn207@gmail.com>
2026-03-31 16:04:09 +0530

    java spring concepts

--- Spring Concepts/Demo/src/main/java/com/neha/spring_using_xml_config/Main.java
+++ Spring Concepts/Demo/src/main/java/com/neha/spring_using_xml_config/Main.java
@@ -1,5 +1,7 @@
 package com.neha.spring_using_xml_config;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,9 +9,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Loading applicationconfig.xml...");
         //using application Context
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/neha/spring_using_xml_config/applicationconfig.xml");
-
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/neha/spring_using_xml_config/applicationconfig.xml");
 //        CourseService courseService = (CourseService) applicationContext.getBean("courseService");
 //        courseService.getCourse();
+
+        DefaultListableBeanFactory defaultListableBeanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(defaultListableBeanFactory);
+        xmlBeanDefinitionReader.loadBeanDefinitions("com/neha/spring_using_xml_config/applicationconfig.xml");
+
+        CourseService courseService = (CourseService) defaultListableBeanFactory.getBean("courseService");
+        courseService.getCourse();
     }
 }
