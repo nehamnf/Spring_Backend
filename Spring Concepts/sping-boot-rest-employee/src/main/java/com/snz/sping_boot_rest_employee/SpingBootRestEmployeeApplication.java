6b5f6385b43c469a164bfaff71b9dd396693316c
NehaMN <nehamn207@gmail.com>
2026-04-15 10:48:38 +0530

    springboot rest

---
+++ Spring Concepts/sping-boot-rest-employee/src/main/java/com/snz/sping_boot_rest_employee/SpingBootRestEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.snz.sping_boot_rest_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootRestEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootRestEmployeeApplication.class, args);
+	}
+
+}
---
+++ Spring Concepts/sping-boot-rest-employee/src/test/java/com/snz/sping_boot_rest_employee/SpingBootRestEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snz.sping_boot_rest_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootRestEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
